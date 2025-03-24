2ccdc5b1f3d00eecd6bc8a01edbc376e848c0342
hexkang <2248951517@qq.com>
2025-03-24 12:54:22 +0800

    online judge base

---
+++ internal/test/helper_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/connorhx/online-judge/internal/helper"
+)
+
+func TestRandGenerate(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	s := ""
+	for i := 0; i < 6; i++ {
+		s += strconv.Itoa(rand.Intn(10))
+	}
+	println(s)
+}
+
+func TestCheckGoCodeValid(t *testing.T) {
+	valid, err := helper.CheckGoCodeValid("../code/code-user/main.go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(valid)
+}
